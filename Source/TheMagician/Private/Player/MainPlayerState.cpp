958639b6df48f14f445858ad077556735a98f6b2
filliperomero <fillipe.romero@gmail.com>
2023-04-05 22:18:06 -0600

    add Main Player State Class

---
+++ Source/TheMagician/Private/Player/MainPlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright Fillipe Romero
+
+
+#include "Player/MainPlayerState.h"
+
+AMainPlayerState::AMainPlayerState()
+{
+	// How often the Server will try to update clients
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/TheMagician/Public/Player/MainPlayerState.h
@@ -0,0 +1,20 @@
+// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "MainPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEMAGICIAN_API AMainPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AMainPlayerState();
+	
+};
