490632bb3e3ae39439bab23575b9f84161bd2fde
filliperomero <fillipe.romero@gmail.com>
2023-04-08 00:25:17 -0600

    add Health, MaxHealth, Mana and MaxMana attributes

--- Source/TheMagician/Private/AbilitySystem/MagicianAttributeSet.cpp
+++ Source/TheMagician/Private/AbilitySystem/MagicianAttributeSet.cpp
@@ -2,4 +2,43 @@
 
 
 #include "AbilitySystem/MagicianAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UMagicianAttributeSet::UMagicianAttributeSet()
+{
+}
+
+void UMagicianAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Register Health to replicate
+	// COND_None = We always want to replicate
+	// REPNOTIFY_Always = If the value is set in the server, it will always replicate even if the variable is the same
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicianAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicianAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicianAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicianAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UMagicianAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	// Informs the Ability System that we're replicating a value
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicianAttributeSet, Health, OldHealth);
+}
+
+void UMagicianAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicianAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UMagicianAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicianAttributeSet, Mana, OldMana);
+}
+
+void UMagicianAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicianAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/TheMagician/Public/AbilitySystem/MagicianAttributeSet.h
+++ Source/TheMagician/Public/AbilitySystem/MagicianAttributeSet.h
@@ -14,4 +14,34 @@ class THEMAGICIAN_API UMagicianAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UMagicianAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/** Begin Attributes */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	/** End Attributes */
+	
 };
