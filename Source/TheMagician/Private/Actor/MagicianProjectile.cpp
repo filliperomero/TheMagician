1ed95dfbc4a4780541c652883783b48ad6bdb4c3
filliperomero <fillipe.romero@gmail.com>
2023-06-03 22:48:12 -0600

    add Magician Projectile class

---
+++ Source/TheMagician/Private/Actor/MagicianProjectile.cpp
@@ -0,0 +1,37 @@
+// Copyright Fillipe Romero
+
+
+#include "Actor/MagicianProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AMagicianProjectile::AMagicianProjectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	SetRootComponent(Sphere);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	ProjectileMovement->InitialSpeed = 550.f;
+	ProjectileMovement->MaxSpeed = 550.f;
+	ProjectileMovement->ProjectileGravityScale = 0.f; // No Gravity
+}
+
+void AMagicianProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
+}
+
+void AMagicianProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
---
+++ Source/TheMagician/Public/Actor/MagicianProjectile.h
@@ -0,0 +1,35 @@
+// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicianProjectile.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+
+UCLASS()
+class THEMAGICIAN_API AMagicianProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMagicianProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+public:	
+
+};
