a9e54467c952b45b7c1c61d1e99f961205275267
filliperomero <fillipe.romero@gmail.com>
2023-06-11 01:07:36 -0600

    fix bug where we were applying 2x a Gameplay Effect because we were having 2 overlap events

--- Source/TheMagician/Private/Character/BaseCharacter.cpp
+++ Source/TheMagician/Private/Character/BaseCharacter.cpp
@@ -11,6 +11,7 @@ ABaseCharacter::ABaseCharacter()
 	PrimaryActorTick.bCanEverTick = false;
 
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetCapsuleComponent()->SetGenerateOverlapEvents(false);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile, ECR_Overlap);
 	GetMesh()->SetGenerateOverlapEvents(true);
