c85eb107d9ca867246ff2ed62f32eb16a8d5405d
filliperomero <fillipe.romero@gmail.com>
2023-06-12 00:49:56 -0600

    add CharacterClassInfo Data Asset and Attributes to different Character Classes

---
+++ Source/TheMagician/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+// Copyright Fillipe Romero
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
+ 
---
+++ Source/TheMagician/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,44 @@
+// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT()
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+UCLASS()
+class THEMAGICIAN_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+};
