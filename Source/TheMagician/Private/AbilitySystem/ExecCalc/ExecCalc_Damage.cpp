09a05142db2a7634220c4cfe9e5d0dbc413a9007
filliperomero <fillipe.romero@gmail.com>
2023-06-17 16:14:01 -0600

    Add Custom Calculation (Execution Calcultion) for Damage with Block Chance implemented

---
+++ Source/TheMagician/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,68 @@
+// Copyright Fillipe Romero
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+#include "MagicianGameplayTags.h"
+#include "AbilitySystem/MagicianAttributeSet.h"
+
+struct MagicianDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);	
+	
+	MagicianDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UMagicianAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UMagicianAttributeSet, BlockChance, Target, false);
+	}
+};
+
+static const MagicianDamageStatics& DamageStatics()
+{
+	static MagicianDamageStatics DStatics;
+	// Every time this function is called, we're going to return the same DStatics since the function and the variable are statics
+	return DStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+	
+	/** Calculation */
+
+	// Get Damage Set by Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FMagicianGameplayTags::Get().Damage);
+
+	// Capture BlockChance on Target, and determine if there was a successful Block
+	// if Blocked, halve the damage
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
+
+	const bool bDamageBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bDamageBlocked ? Damage / 2.f : Damage;
+
+	const FGameplayModifierEvaluatedData EvaluatedData(UMagicianAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+}
