2e7147bbcb8f41253c029acf077b9daad99f5da9
filliperomero <fillipe.romero@gmail.com>
2023-06-21 16:19:07 -0600

    add MagicianAbilityTypes to be used to override Types for Abilities

---
+++ Source/TheMagician/Public/MagicianAbilityTypes.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "MagicianAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMagicianGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const override	{ return FGameplayEffectContext::StaticStruct(); }
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit { false };
+	
+	UPROPERTY()
+	bool bIsCriticalHit { false };
+};
