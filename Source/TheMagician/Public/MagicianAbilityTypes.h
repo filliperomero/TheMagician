a4bad13da7643fab3912c33befdc0899140d6bf3
filliperomero <fillipe.romero@gmail.com>
2023-06-21 22:45:22 -0600

    Add Struct OpsTypeTraits template so we can define an enum on it

--- Source/TheMagician/Public/MagicianAbilityTypes.h
+++ Source/TheMagician/Public/MagicianAbilityTypes.h
@@ -18,6 +18,18 @@ public:
 	/** Returns the actual struct used for serialization, subclasses must override this! */
 	virtual UScriptStruct* GetScriptStruct() const override	{ return FGameplayEffectContext::StaticStruct(); }
 
+	virtual FGameplayEffectContext* Duplicate() const override
+	{
+		FGameplayEffectContext* NewContext = new FGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
 
@@ -27,4 +39,15 @@ protected:
 	
 	UPROPERTY()
 	bool bIsCriticalHit { false };
+};
+
+// This Template is defining if our GameplayEffectContext has Net Serialization and if can handle HitResult copy
+template<>
+struct TStructOpsTypeTraits<FMagicianGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FMagicianGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true // Necessary so that TSharedPtr<FHitResult> Data is copied around
+	};
 };
