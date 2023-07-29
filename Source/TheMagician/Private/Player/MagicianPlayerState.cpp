d36348221f97c93205c5f21a91b90a769fcb1065
filliperomero <fillipe.romero@gmail.com>
2023-07-29 11:53:29 -0600

    Add XP to Player State

--- Source/TheMagician/Private/Player/MagicianPlayerState.cpp
+++ Source/TheMagician/Private/Player/MagicianPlayerState.cpp
@@ -24,6 +24,7 @@ void AMagicianPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(AMagicianPlayerState, Level);
+	DOREPLIFETIME(AMagicianPlayerState, XP);
 }
 
 UAbilitySystemComponent* AMagicianPlayerState::GetAbilitySystemComponent() const
@@ -31,6 +32,36 @@ UAbilitySystemComponent* AMagicianPlayerState::GetAbilitySystemComponent() const
 	return AbilitySystemComponent;
 }
 
+void AMagicianPlayerState::AddToLevel(int32 InLevel)
+{
+	Level += InLevel;
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AMagicianPlayerState::SetLevel(int32 InLevel)
+{
+	Level = InLevel;
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AMagicianPlayerState::AddToXP(int32 InXP)
+{
+	XP += InXP;
+	OnXPChangedDelegate.Broadcast(XP);
+}
+
+void AMagicianPlayerState::SetXP(int32 InXP)
+{
+	XP = InXP;
+	OnXPChangedDelegate.Broadcast(XP);
+}
+
 void AMagicianPlayerState::OnRep_Level(int32 OldLevel)
 {
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AMagicianPlayerState::OnRep_XP(int32 OldXP)
+{
+	OnXPChangedDelegate.Broadcast(XP);
 }
--- Source/TheMagician/Public/Player/MagicianPlayerState.h
+++ Source/TheMagician/Public/Player/MagicianPlayerState.h
@@ -10,9 +10,8 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
-/**
- * 
- */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32 /*StatValue*/)
+
 UCLASS()
 class THEMAGICIAN_API AMagicianPlayerState : public APlayerState, public IAbilitySystemInterface
 {
@@ -22,6 +21,14 @@ public:
 	AMagicianPlayerState();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	void AddToLevel(int32 InLevel);
+	void SetLevel(int32 InLevel);
+	void AddToXP(int32 InXP);
+	void SetXP(int32 InXP);
+
+	FOnPlayerStatChangedSignature OnXPChangedDelegate;
+	FOnPlayerStatChangedSignature OnLevelChangedDelegate;
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -39,8 +46,15 @@ private:
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP { 0 };
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
+
 public:
 	FORCEINLINE UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetXP() const { return XP; }
 	
 };
