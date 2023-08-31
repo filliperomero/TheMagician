48c8a01630c0058b9a46d36be1820023ea9d1227
filliperomero <fillipe.romero@gmail.com>
2023-08-30 23:50:07 -0600

    Add EquippedRow_Button Widget, make all Widget's have their controller set and add a delegate event listener on EquippedRow_Button widget

--- Source/TheMagician/Private/MagicianGameplayTags.cpp
+++ Source/TheMagician/Private/MagicianGameplayTags.cpp
@@ -36,6 +36,8 @@ void FMagicianGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.2"), FString("Input Tag for 2 Key"));
 	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.3"), FString("Input Tag for 3 Key"));
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.4"), FString("Input Tag for 4 Key"));
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Passive.1"), FString("Input Tag for Passive Ability 1"));
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Passive.2"), FString("Input Tag for Passive Ability 2"));
 
 	/** Damage Tags */
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Damage"), FString("Damage"));
